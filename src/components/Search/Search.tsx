b2dcae8ad70c33444ae08f27de3e9ca0971ea96f
Joao <joaoviniciusvitral@hotmail.com>
2022-08-30 21:16:24 -0300

    handle enter

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -5,11 +5,18 @@ import { useApp } from '../../context';
 export const Search: React.FC = () => {
     const { setSelectedOptions, selectedOptions, handleSearch }: any = useApp()
   
+    const handleEnter = (e: any) => {
+        if (e.code === 'Enter') {
+            handleSearch()
+        }
+    }
+
     return (
         <Flex margin='15px auto'>
             <Input
                 placeholder='Search'
-                onChange={(e: any) => setSelectedOptions({...selectedOptions, name: e.target.value})}
+                onChange={(e: any) => setSelectedOptions({ ...selectedOptions, name: e.target.value })}
+                onKeyUp={handleEnter}
                 value={selectedOptions?.name}
             />
             <Button onClick={handleSearch}>Search</Button>
